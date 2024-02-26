9ddbf292864438b804eb41c913aab7dffef306e2
justin etienne <juetienne7@gmail.com>
2024-02-26 16:51:48 +0900

    routes and item validation done

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   has_many :planners
 
   validates :name, presence: true
+  # validates :type, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
+  validates :color, presence: true
+  # validates :shop_name, presence: true
+  validates :shop_url, presence: true
+  # validates :dimensions, presence: true?
+  # validates :shop_item_id, presence: true?
 end
