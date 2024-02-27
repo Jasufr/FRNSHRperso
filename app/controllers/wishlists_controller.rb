5e19d4ff6690e52d366800fca493a8df5ea42637
justin etienne <juetienne7@gmail.com>
2024-02-27 14:21:13 +0900

    search beginning

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,9 +1,10 @@
 class WishlistsController < ApplicationController
 
 def index
-  @wishlists = policy_scope(wishlist)
+  @wishlists = policy_scope(Wishlist)
   @room = Room.find(params[:room_id])
   @wishlists = @wishlists.where(room: @room)
+  @items = Item.all
 end
 
 def create
