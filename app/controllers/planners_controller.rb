934d23fb9e2c4a80bc59422de76f3be3f4eaa59d
justin etienne <juetienne7@gmail.com>
2024-02-27 16:36:58 +0900

    planners container added

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -3,5 +3,6 @@ class PlannersController < ApplicationController
     @planners = policy_scope(Planner)
     @room = Room.find(params[:room_id])
     @planners = @planners.where(room: @room)
+    @wishlists = @room.wishlists
   end
 end
