f8e3018786c4d777185c218bfa532c7e5af870aa
Nick Westbrook <ntw3001@gmail.com>
2024-02-27 15:49:03 +0900

    wishbar cards exist

--- app/policies/wishlist_policy.rb
+++ app/policies/wishlist_policy.rb
@@ -2,7 +2,8 @@ class WishlistPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user:)
+      scope.all
+      # where(user: user)
     end
   end
 end
