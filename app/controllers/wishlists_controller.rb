e719ddf4deb625445fe324248b67c623fa3bae11
Nick Westbrook <ntw3001@gmail.com>
2024-02-27 13:22:37 +0900

    wishlist pa]ge can be found

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,18 +1,32 @@
 class WishlistsController < ApplicationController
 
 def index
-  @wishlists = policy_scope(wishlist)
+  @wishlists = policy_scope(Wishlist)
   @room = Room.find(params[:room_id])
   @wishlists = @wishlists.where(room: @room)
 end
 
 def create
-  @wishlist = Wishlist.new
+  @wishlist = Wishlist.new(wishlist_params)
+  authorize @wishlist
+  @room = Room.find(params[:room_id])
+  @wishlist.room = @room
+  # if @wishlist.save
+  #   redirect_to wishlist_path(@wishlist) - don't redirect, just plop it in the partial
+  # else
+  #   render :new, status: :unprocessable_entity
+  # end
 end
 
 def destroy
   @wishlist = Wishlist.find(params[:id])
   @wishlist.destroy
+end
+
+private
+
+def wishlist_params
+  params.require(:wishlist).permit(:item_id)
 end
 
 end
