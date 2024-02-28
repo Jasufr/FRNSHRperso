b0cea4adaebb8ced3f3a9f99d6219e303e1afd66
justin etienne <juetienne7@gmail.com>
2024-02-28 17:23:50 +0900

    narrow search works beside the color selection

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,12 +4,28 @@ class WishlistsController < ApplicationController
     @wishlists = policy_scope(Wishlist)
     @room = Room.find(params[:room_id])
     @wishlists = @wishlists.where(room: @room)
+    # To do: turn this one into Items that matches the room
+    @items = Item.all
+
     if params[:query].present?
       # To do: add also Items that matches the room
-      @items = Item.search_by_name(params[:query])
-    else
-      # To do: turn this one into Items that matches the room
-      @items = Item.all
+      @items = @items.search_by_name(params[:query])
+    end
+    if params[:room_type].present?
+      room_type = params[:room_type]
+      @items = @items.where(furniture_type: Item::ROOM_ITEMS[room_type])
+    end
+    # if params[:furniture_type].present?
+    #   @items = @items.where(furniture_type: params[:furniture_type])
+    # end
+    if params[:price_range].present?
+      sql_subquery = "price >= ? AND price <= ?"
+      @items = @items.where(sql_subquery, params[:price_range].split(",")[0].to_i, params[:price_range].split("-")[1].to_i)
+    end
+
+    if params[:width].present? && params[:width] != "-1"
+      sql_subquery = "x_dimension >= ? AND x_dimension <= ?"
+      @items = @items.where(sql_subquery, (params[:width].to_i - 20), (params[:width].to_i + 20))
     end
   end
 
