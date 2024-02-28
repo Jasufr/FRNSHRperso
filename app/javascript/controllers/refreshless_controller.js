f580c6adf3e1e7e89237f0e78025b3ed2e1462b3
Ruby Tam <pkamkam@gmail.com>
2024-02-28 17:29:08 +0900

    ajax

--- app/javascript/controllers/refreshless_controller.js
+++ app/javascript/controllers/refreshless_controller.js
@@ -3,13 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="refreshless"
 export default class extends Controller {
 
-  static targets = ["form"]
+  static targets = ["form", "display"]
 
   connect() {
     console.log("connected")
   }
 
   add(event) {
+    event.preventDefault();
     console.log("An item is added to planner")
+    console.log(event.currentTarget);
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(event.currentTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data.html)
+        console.log(this.displayTarget)
+        this.displayTarget.insertAdjacentHTML("beforeend", data.html)
+      })
   }
 }
