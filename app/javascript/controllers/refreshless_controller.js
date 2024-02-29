79bfe4fd4b471fac0543c3330759655c25c40167
Ruby Tam <pkamkam@gmail.com>
2024-02-29 10:30:20 +0900

    delete by simple form

--- app/javascript/controllers/refreshless_controller.js
+++ app/javascript/controllers/refreshless_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="refreshless"
 export default class extends Controller {
 
-  static targets = ["form", "display"]
+  static targets = ["form", "display","remove"]
 
   connect() {
     console.log("connected")
@@ -25,4 +25,22 @@ export default class extends Controller {
         this.displayTarget.insertAdjacentHTML("beforeend", data.html)
       })
   }
+
+  // remove(event) {
+  //   event.preventDefault();
+  //   console.log("An item is deleted from planner");
+  //   console.log(event.currentTarget);
+  //   console.log(this.removeTarget.href)
+  //   fetch(this.removeTarget.href, {
+  //     method: "DELETE",
+  //     headers: { "Accept": "application/json" }
+  //   })
+  //     .then(response => {
+  //       console.log(response);
+  //       response.json()
+  //     })
+  //     .then((data) => {
+
+  //     })
+  // }
 }
