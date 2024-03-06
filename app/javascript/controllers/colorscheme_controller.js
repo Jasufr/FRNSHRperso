bdcac050a5ae6f44ef08f53be72d044f20f5e8f2
Ruby Tam <pkamkam@gmail.com>
2024-03-06 18:37:56 +0900

    add to 4th cards, will do styling on them tomorrow

--- app/javascript/controllers/colorscheme_controller.js
+++ app/javascript/controllers/colorscheme_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="colorscheme"
 export default class extends Controller {
-  static targets = ["usercolor", "displayAnalogic","analogic","displayMonochrome","monochrome"];
+  static targets = ["usercolor", "displayArea","displayAnalogic","displayMonochrome","displayMonochromeLight","displayQuad"];
 
   connect() {
     console.log("js connected");
@@ -32,12 +32,15 @@ export default class extends Controller {
 
 
   suggestion(event){
-
+    this.displayAreaTarget.innerHTML = ""; // reset everthing
     console.log("color is connected");
     const usercolor = this.usercolorTarget.value.slice(1);
     console.log(usercolor);
+    const schemeButton = `<button data-action="click->colorscheme#select">Pick this color scheme</button>`;
+
   // 1st color scheme
-    this.displayAnalogicTarget.innerHTML = "";
+
+    this.displayAreaTarget.insertAdjacentHTML('beforeend','<div data-colorscheme-target="displayAnalogic" class = "scheme-card"></div>');
     this.displayAnalogicTarget.insertAdjacentHTML('beforeend',"<p>1st Suggested Color Scheme</p>");
     fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=analogic&count=5`)
   .then(response => {
@@ -55,12 +58,11 @@ export default class extends Controller {
       const colorPicker = `<input type="color" name="" value="${colorHex}">`;
       this.displayAnalogicTarget.insertAdjacentHTML('beforeend',colorPicker);
     });
-const analogicButton = `<button data-action="click->colorscheme#select">Pick this color scheme</button>`;
-this.displayAnalogicTarget.insertAdjacentHTML('beforeend',analogicButton)
+this.displayAnalogicTarget.insertAdjacentHTML('beforeend',schemeButton)
   })
 
 // 2nd color scheme
-this.displayMonochromeTarget.innerHTML = "";
+this.displayAreaTarget.insertAdjacentHTML('beforeend','<div data-colorscheme-target="displayMonochrome" class = "scheme-card"></div>');
 this.displayMonochromeTarget.insertAdjacentHTML('beforeend',"<p>2nd Suggested Color Scheme</p>");
   fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=monochrome&count=5`)
   .then(response => {
@@ -78,20 +80,52 @@ this.displayMonochromeTarget.insertAdjacentHTML('beforeend',"<p>2nd Suggested Co
       const colorPicker = `<input type="color" name="" value="${colorHex}">`;
       this.displayMonochromeTarget.insertAdjacentHTML('beforeend',colorPicker);
     });
-const monochromeButton = `<button data-action="click->colorscheme#select">Pick this color scheme</button>`;
-this.displayMonochromeTarget.insertAdjacentHTML('beforeend',monochromeButton)
+this.displayMonochromeTarget.insertAdjacentHTML('beforeend',schemeButton)
   })
 
-// reset the selector to not selected
-const allDivs = document.querySelectorAll('.scheme-card');
-allDivs.forEach(div => {
-  div.style.border = '0';
-  const allInputs = div.querySelectorAll('input[type="color"]');
-  allInputs.forEach(input => {
-      input.setAttribute("name", "");
-  });
-});
+// 3rd color scheme
+this.displayAreaTarget.insertAdjacentHTML('beforeend','<div data-colorscheme-target="displayMonochromeLight" class = "scheme-card"></div>');
+this.displayMonochromeLightTarget.insertAdjacentHTML('beforeend',"<p>3nd Suggested Color Scheme</p>");
+  fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=monochrome-light&count=5`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // This will return an long array
+    const scheme = data.colors;
+    scheme.forEach(color => {
+      const colorHex = color.hex.value;
+      console.log(colorHex); // This will log the hexcode of each color
+      const colorPicker = `<input type="color" name="" value="${colorHex}">`;
+      this.displayMonochromeLightTarget.insertAdjacentHTML('beforeend',colorPicker);
+    });
+this.displayMonochromeLightTarget.insertAdjacentHTML('beforeend',schemeButton)
+  })
 
+  // 4th color scheme
+this.displayAreaTarget.insertAdjacentHTML('beforeend','<div data-colorscheme-target="displayQuad" class = "scheme-card"></div>');
+this.displayQuadTarget.insertAdjacentHTML('beforeend',"<p>3nd Suggested Color Scheme</p>");
+  fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=quad&count=5`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // This will return an long array
+    const scheme = data.colors;
+    scheme.forEach(color => {
+      const colorHex = color.hex.value;
+      console.log(colorHex); // This will log the hexcode of each color
+      const colorPicker = `<input type="color" name="" value="${colorHex}">`;
+      this.displayQuadTarget.insertAdjacentHTML('beforeend',colorPicker);
+    });
+this.displayQuadTarget.insertAdjacentHTML('beforeend',schemeButton)
+  })
 
   }
 }
