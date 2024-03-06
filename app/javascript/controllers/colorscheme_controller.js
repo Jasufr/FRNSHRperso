754daf8a830041fff2b20db19a2460265fbed947
Ruby Tam <pkamkam@gmail.com>
2024-03-06 15:19:07 +0900

    try to have 2nd scheme

--- app/javascript/controllers/colorscheme_controller.js
+++ app/javascript/controllers/colorscheme_controller.js
@@ -2,11 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="colorscheme"
 export default class extends Controller {
-  static targets = ["usercolor", "colorDisplay"];
+  static targets = ["usercolor", "displayAnalogic","analogic"];
 
   connect() {
     console.log("js connected");
   }
+  selectAnalogic(event) {
+    event.preventDefault();
+    this.displayAnalogicTarget.style.border = "2px solid white";
+    this.analogicTargets.forEach(element => {
+      element.setAttribute("name", "room[palette][]");
+    });
+
+}
   roomchecking(event){
     console.log("room is connected");
   }
@@ -14,7 +22,7 @@ export default class extends Controller {
     console.log("color is connected");
     const usercolor = this.usercolorTarget.value.slice(1);
     console.log(usercolor);
-    this.colorDisplayTarget.innerHTML = "";
+    this.displayAnalogicTarget.innerHTML = "";
     fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=analogic&count=5`)
   .then(response => {
     if (!response.ok) {
@@ -28,10 +36,11 @@ export default class extends Controller {
     scheme.forEach(color => {
       const colorHex = color.hex.value;
       console.log(colorHex); // This will log the hexcode of each color
-      const colorPicker = `<input type="color" name="room[palette][]" value="${colorHex}">`;
-      this.colorDisplayTarget.insertAdjacentHTML('beforeend',colorPicker)
+      const colorPicker = `<input type="color" data-colorscheme-target="analogic" name="" value="${colorHex}">`;
+      this.displayAnalogicTarget.insertAdjacentHTML('beforeend',colorPicker);
     });
-
+const analogicButton = `<button data-action="click->colorscheme#selectAnalogic">Pick this</button>`;
+this.displayAnalogicTarget.insertAdjacentHTML('beforeend',analogicButton)
   })
   }
 }
