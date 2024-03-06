8daa0798031aac9c20e6b496163aee97cebb5ada
Ruby Tam <pkamkam@gmail.com>
2024-03-06 12:22:30 +0900

    can return the color hex code

--- app/javascript/controllers/colorscheme_controller.js
+++ app/javascript/controllers/colorscheme_controller.js
@@ -14,5 +14,23 @@ export default class extends Controller {
     console.log("color is connected");
     const usercolor = this.usercolorTarget.value.slice(1);
     console.log(usercolor);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${usercolor}&format=json&mode=analogic&count=5`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // This will return an long array
+    const scheme = data.colors;
+    scheme.reverse().forEach(color => {
+      const colorHex = color.hex.value;
+      console.log(colorHex); // This will log the hexcode of each color
+      const colorPicker = "";
+      this.usercolorTarget.insertAdjacentHTML('afterend',colorHex)
+    });
+
+  })
   }
 }
